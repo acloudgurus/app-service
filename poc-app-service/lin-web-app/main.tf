17850bcba0882c2086a75d05f20d02d591c8a1b3
acloudgurus <66688065+acloudgurus@users.noreply.github.com>
2024-06-08 03:43:47 +0530

    Add files via upload

--- poc-app-service/lin-web-app/main.tf
+++ poc-app-service/lin-web-app/main.tf
@@ -1,1 +1,22 @@
+# 
+# This module is owned by SRE LandingZone's Team 
+# 
 
+module "linux_web_app" {
+  source  = "../../linux_web_app"
+  app_service_name    = "aasp2"
+  location            = "East US"
+  resource_group_name = "test-rg"
+  service_plan_id     = "/subscriptions/xxxxxx/resourceGroups/test-rg/providers/Microsoft.Web/serverFarms/testasp"
+
+  app_settings = {
+    DB = "WMSRE"
+  }
+
+  site_config = {
+    application_stack = {
+      current_stack  = "dotnet"
+      dotnet_version = "v7.0"
+    }
+  }
+}
