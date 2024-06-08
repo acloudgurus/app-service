3c25eba7958895ddb0dc26052e8b4f574fe1d74e
acloudgurus <66688065+acloudgurus@users.noreply.github.com>
2024-06-08 05:54:56 +0530

    Update provider.tf

--- linux_web_app/provider.tf
+++ linux_web_app/provider.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "240be98a-3c73-49fa-bea8-d8fa590c6389"
+ subscription_id = "4a9ac668-c3d5-4a20-859a-c680184367b8"
   skip_provider_registration = true 
-  tenant_id = "39981c07-b49b-43c2-a3ad-ffb43ab32375"
-  #client_id = "e5a77fc6-9445-4d05-9cce-d1ec0fd97b96"
-  #client_secret = "7ds8Q~uB7a2hJb5Gho78QDEt94OZB7CcpXyVucAw"
+  tenant_id = "66167829-0104-44d5-9f0c-90583021fcab"
+  client_id = "2a06646a-bda0-49f7-bd72-a714e1dfb122"
+  client_secret = "PHg8Q~tW4pWQJ2EUGS9ZWW3hGH~6FTNUbPTGmbeN"
 
  features {}
 }
