5496a5a974ba44f01ea96fd7de3ae3b1ef4c3612
acloudgurus <66688065+acloudgurus@users.noreply.github.com>
2024-06-08 05:58:14 +0530

    Update main.tf

--- poc-app-service/lin-web-app/main.tf
+++ poc-app-service/lin-web-app/main.tf
@@ -7,7 +7,7 @@ module "linux_web_app" {
   app_service_name    = "aasp2"
   location            = "East US"
   resource_group_name = "test-rg"
-  service_plan_id     = "/subscriptions/#{subscriptionID}/resourcegroups/test-rg/providers/Microsoft.Web/serverFarms/testasp"
+  service_plan_id     = "/subscriptions/4a9ac668-c3d5-4a20-859a-c680184367b8/resourceGroups/test-rg/providers/Microsoft.Web/serverFarms/testasp""
 
   app_settings = {
     DB = "WMSRE"
