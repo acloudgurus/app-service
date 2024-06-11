d3890e045e8fa7e6097fa4630cb36d11ab379a32
acloudgurus <66688065+acloudgurus@users.noreply.github.com>
2024-06-12 01:22:34 +0530

    Update provider.tf

--- linux_web_app/provider.tf
+++ linux_web_app/provider.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-  #subscription_id = "4a9ac668-c3d5-4a20-859a-c680184367b8"
-  #skip_provider_registration = true 
-  #tenant_id = "66167829-0104-44d5-9f0c-90583021fcab"
-  #client_id = "2a06646a-bda0-49f7-bd72-a714e1dfb122"
-  #client_secret = "PHg8Q~tW4pWQJ2EUGS9ZWW3hGH~6FTNUbPTGmbeN"
+  subscription_id = "#{AzureAccount.SubscriptionId}"
+  skip_provider_registration = true 
+  tenant_id = "#{AzureAccount.TenantID}"
+  client_id = "#{AzureAccount.ApplicationID}"
+  client_secret = "#{AzureAccount.Password}"
 
  features {}
 }
