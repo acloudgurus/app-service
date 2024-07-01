4967e566d625937ff2574fa811bb080bf752553d
acloudgurus <66688065+acloudgurus@users.noreply.github.com>
2024-07-02 01:26:55 +0530

    Update appinsights.tf

--- linux_web_app/appinsights.tf
+++ linux_web_app/appinsights.tf
@@ -3,7 +3,7 @@ resource "azurerm_application_insights" "app_insights" {
 
   name                = var.application_insights_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.resource_group_name}"
 
   workspace_id     = var.application_insights_log_analytics_workspace_id
   application_type = var.application_insights_type
